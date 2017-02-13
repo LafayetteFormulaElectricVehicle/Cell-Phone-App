c5b0b881084c22c24adec611fbc6834a44d97926
Kemal Dilsiz <ktdilsiz@gmail.com>
2017-02-13 10:28:38 -0500

    Test environment created for Kemal

---
+++ testKemal/cellapptest/app/src/main/java/com/lfev2017/ktdilsiz/cellapptest/FrontScreen.java
@@ -0,0 +1,13 @@
+package com.lfev2017.ktdilsiz.cellapptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrontScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_screen);
+    }
+}
