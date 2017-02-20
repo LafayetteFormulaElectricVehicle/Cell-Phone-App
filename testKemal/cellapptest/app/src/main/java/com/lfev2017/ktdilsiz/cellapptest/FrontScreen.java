81de7fc07b73bd75d155b7b84ff41351ed5a9f56
Kemal Dilsiz <ktdilsiz@gmail.com>
2017-02-20 11:24:29 -0500

    added Draggable View

--- testKemal/cellapptest/app/src/main/java/com/lfev2017/ktdilsiz/cellapptest/FrontScreen.java
+++ testKemal/cellapptest/app/src/main/java/com/lfev2017/ktdilsiz/cellapptest/FrontScreen.java
@@ -2,12 +2,24 @@ package com.lfev2017.ktdilsiz.cellapptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class FrontScreen extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.lfev2017.ktdilsiz.cellapptest.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_screen);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
