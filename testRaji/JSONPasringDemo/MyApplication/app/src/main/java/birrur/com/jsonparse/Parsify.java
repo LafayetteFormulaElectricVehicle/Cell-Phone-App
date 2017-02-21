1d9203eefdec0578be1df1977f4d7e018a524bfd
birrur <birrur@lafayette.edu>
2017-02-21 17:51:48 -0500

    Fixed Issues with Parsing

--- testRaji/JSONPasringDemo/MyApplication/app/src/main/java/birrur/com/jsonparse/Parsify.java
+++ testRaji/JSONPasringDemo/MyApplication/app/src/main/java/birrur/com/jsonparse/Parsify.java
@@ -1,13 +1,13 @@
 package birrur.com.jsonparse;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,10 +33,18 @@ public class Parsify extends AppCompatActivity {
 
 
         TextView tvRPME = (TextView) findViewById(R.id.tvRPME);
-        TextView tvSpeedE = (TextView) findViewById(R.id.tvRPME);
+        TextView tvSpeedE = (TextView) findViewById(R.id.tvVE);
+        TextView tvVE = (TextView) findViewById(R.id.tvVE);
+        TextView tvIE = (TextView) findViewById(R.id.tvIE);
+        TextView tvSocE = (TextView) findViewById(R.id.tvSocE);
+        TextView tvOnE = (TextView) findViewById(R.id.tvOnE);
 
         tvRPME.setText(dataSet.getDashboard().getRpm().toString());
         tvSpeedE.setText(dataSet.getDashboard().getSpeed().toString());
+        tvVE.setText(dataSet.getTsv().getVoltage().toString());
+        tvIE.setText(dataSet.getTsv().getCurrent().toString());
+        tvSocE.setText(dataSet.getTsv().getStateOfCharge().toString());
+        tvOnE.setText(dataSet.getTsv().getEnabled().toString());
     }
 
     public String readSon(){
