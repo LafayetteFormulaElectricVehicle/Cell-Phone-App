fe81130e73000a13c7522d27e353e46a4e5ed798
birrur <birrur>
2017-03-07 16:09:32 -0500

    Added new version of app. Improved skeleton.

---
+++ trunk/LFEVCell/app/src/main/java/com/lafayette/lfevcell/CanvasFragment.java
@@ -0,0 +1,22 @@
+package com.lafayette.lfevcell;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by birrur on 3/7/2017.
+ */
+
+public class CanvasFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.canvas_layout,null);
+    }
+
+}
---
+++ trunk/LFEVCell/app/src/main/java/com/lafayette/lfevcell/TabFragment.java
@@ -0,0 +1,82 @@
+package com.lafayette.lfevcell;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by birrur on 3/7/2017.
+ */
+
+public class TabFragment extends Fragment{
+
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 2 ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View x =  inflater.inflate(R.layout.tab_layout,null);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new DashFragment();
+                case 1 : return new CanvasFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "Dash";
+                case 1 :
+                    return "Canvas";
+            }
+            return null;
+        }
+    }
+}
+
